89d09a872bf07b025881e40f746517ed4d541042
NotifyOne <anton220812@gmail.com>
2019-07-17 15:32:12 +0300

    Add yearToArray function

---
+++ yearToArray/yearToArray.php
@@ -0,0 +1,30 @@
+<?php
+namespace yearToArray;
+
+function toArray(array $years) {
+  $parsed = [];
+
+  foreach ($years as $key=>$year) {
+    $parsed[] = [explode('-', $key), $year];
+  }
+
+  $arr = [];
+  foreach ($parsed as $parse) {
+    $arr[$parse[0][0]][$parse[0][1]][$parse[0][2]] = $parse[1];
+  }
+  unset($parsed);
+
+//  var_dump($arr);
+  $return = [];
+
+  foreach ($arr as $key=>$aa) {
+    foreach ($aa as $key1=>$a) {
+      $return[] = array_merge([
+        'Year' => $key1,
+        'Form' => $key,
+      ], $a);
+    }
+  }
+
+  return $return;
+}
