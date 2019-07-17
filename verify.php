7d5cef14025e07f9a88671ef7494ca22a3a26b62
NotifyOne <anton220812@gmail.com>
2019-07-17 16:28:49 +0300

    Add validation function

---
+++ verify.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'yearToArray/yearToArray.php';
+
+function validate($arrayToValidate) {
+  foreach ($arrayToValidate as $table) {
+    $start = false;
+    $break = false;
+    foreach ($table as $t) {
+      foreach ($t as $key=>$elem) {
+        if (
+          ('Q1' == $key)
+          || ('Q2' == $key)
+          || ('Q3' == $key)
+          || ('Q4' == $key)
+          || ('YDT' == $key)
+        ) continue;
+        if ($elem != '') {
+          if ($break)
+            return 'Invalid';
+          $start = true;
+        } else {
+          if (!$start) {
+            $start = false;
+          } else {
+            $break = true;
+          }
+        }
+      }
+
+    }
+  }
+  return 'Valid';
+}
+
+if (isset($_POST['arr'])) {
+  $tables = yearToArray\tzParseTableYear(yearToArray\toArrayTZ($_POST['arr']));
+
+  echo validate($tables);
+}
